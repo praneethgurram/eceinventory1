cef74c176eda1dac24afeff63bbef25e96854b9c
abhijeet <abhijeet.tawar007@gmail.com>
2017-04-11 11:03:50 -0400

    crude xml parser

---
+++ AbhiXMLParsing/app/src/main/java/com/example/abhixmlparsing/Item.java
@@ -0,0 +1,125 @@
+package com.example.abhixmlparsing;
+
+/**
+ * Created by abhi on 3/30/17.
+ */
+
+public class Item
+{
+   public String AcqDate = " ";
+    public String Amt = " ";
+    public String AssetType = "";
+    public String AtypTitle = "";
+    public String Bldg = "";
+    public String Condition = "";
+    public String Custodian = "";
+    public String Description = "";
+    public String Designation = "";
+    public String LastInvDate = "";
+    public String Manufacturer = "";
+    public String Model = "";
+    public String OrgnCode = "";
+    public String OrgnTitle = "";
+    public String Owner = "";
+    public String Ownership = "";
+    public String PO = "";
+    public String Ptag = "";
+    public String Room = "";
+    public String SN = "";
+    public String SchevYear = "";
+    public String SortRoom = "";
+    public String TagType = "";
+
+    public String getAcqDate() {
+        return AcqDate;
+    }
+
+    public String getAssetType() {
+        return AssetType;
+    }
+
+    public String getAtypTitle() {
+        return AtypTitle;
+    }
+
+    public String getCondition() {
+        return Condition;
+    }
+
+    public String getTagType() {
+        return TagType;
+    }
+
+    public String getAmt() {
+        return Amt;
+    }
+
+    public String getBldg() {
+        return Bldg;
+    }
+
+    public String getCustodian() {
+        return Custodian;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public String getDesignation() {
+        return Designation;
+    }
+
+    public String getLastInvDate() {
+        return LastInvDate;
+    }
+
+    public String getManufacturer() {
+        return Manufacturer;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public String getOrgnCode() {
+        return OrgnCode;
+    }
+
+    public String getOrgnTitle() {
+        return OrgnTitle;
+    }
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public String getOwnership() {
+        return Ownership;
+    }
+
+    public String getPO() {
+        return PO;
+    }
+
+    public String getPtag() {
+        return Ptag;
+    }
+
+    public String getRoom() {
+        return Room;
+    }
+
+    public String getSN() {
+        return SN;
+    }
+
+    public String getSortRoom() {
+        return SortRoom;
+    }
+
+    public String getSchevYear()
+    {
+        return SchevYear;
+    }
+}
